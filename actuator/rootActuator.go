5e8587847e6e52af2ad594dea298e3d8248e5591
JitinSynectiks <jitin.kumar@synectiks.com>
2023-04-19 16:03:49 +0530

    api test

--- actuator/rootActuator.go
+++ actuator/rootActuator.go
@@ -5,9 +5,10 @@ import (
 	"github.com/Appkube-awsx/awsx-lambda/authenticater"
 	"github.com/Appkube-awsx/awsx-lambda/client"
 	"github.com/Appkube-awsx/awsx-lambda/controllers"
+	"github.com/aws/aws-sdk-go/service/lambda"
 )
 
-func LambdaListActuator(marker string, all bool, vaultUrl string, accountId string, region string, acKey string, secKey string, crossAccountRoleArn string, externalId string) {
+func LambdaListActuator(marker string, all bool, vaultUrl string, accountId string, region string, acKey string, secKey string, crossAccountRoleArn string, externalId string) []*lambda.FunctionConfiguration {
 
 	// For request from API
 	authFlag := authenticater.AuthenticateData(vaultUrl, accountId, region, acKey, secKey, crossAccountRoleArn, externalId)
@@ -23,11 +24,7 @@ func LambdaListActuator(marker string, all bool, vaultUrl string, accountId stri
 
 	lambdaClient := client.GetClient()
 
-	if all {
-		functionList := controllers.GetAllLambdaList(lambdaClient)
-		fmt.Println("List of all lambda functions", functionList)
-	} else {
-		functionList := controllers.GetLambdaList(lambdaClient, marker)
-		fmt.Println("List of by marker lambda functions", functionList)
-	}
+	functionList := controllers.GetAllLambdaList(lambdaClient)
+	fmt.Println("List of all lambda functions", functionList)
+	return functionList
 }
