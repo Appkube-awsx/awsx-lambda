4c40194ac01ea0932feec40995988142378188ce
ManojSynectiks <manoj.sharma@synectiks.com>
2023-10-05 13:22:59 +0530

    retrieving tags of each lambda function

--- controllers/root-controller.go
+++ controllers/root-controller.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/Appkube-awsx/awsx-common/client"
 	"github.com/Appkube-awsx/awsx-lambda/services"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/lambda"
+	"log"
 )
 
 // AllLambdaListController for all lambda list
@@ -33,4 +36,45 @@ func LambdaListController(marker string, auth client.Auth) *lambda.ListFunctions
 
 	fmt.Println("List of lambda functions", functionList)
 	return functionList
+}
+
+type LambdaObj struct {
+	Function *lambda.FunctionConfiguration `json:"function"`
+	Tags     interface{}                   `json:"tags"`
+}
+
+func AllLambdaFunctionsWithTagsController(auth client.Auth) (string, error) {
+	fmt.Println("Request to get list of lambda functions with tags")
+	lambdaClient := client.GetClient(auth, client.LAMBDA_CLIENT).(*lambda.Lambda)
+
+	functionList := services.GetAllLambdaList(lambdaClient)
+
+	allLambda := []LambdaObj{}
+	for _, function := range functionList {
+		input := &lambda.ListTagsInput{
+			Resource: aws.String(*function.FunctionArn),
+		}
+		resp, err := lambdaClient.ListTags(input)
+		if err != nil {
+			fmt.Println("Error: in getting lambda tags", err)
+			continue
+		}
+
+		allTags := []map[string]string{}
+		for key, val := range resp.Tags {
+			tagsMap := make(map[string]string)
+			tagsMap["Key"] = key
+			tagsMap["Value"] = *val
+
+			allTags = append(allTags, tagsMap)
+		}
+		lambdaObj := LambdaObj{
+			Function: function,
+			Tags:     allTags,
+		}
+		allLambda = append(allLambda, lambdaObj)
+	}
+	jsonData, err := json.Marshal(allLambda)
+	log.Println(string(jsonData))
+	return string(jsonData), err
 }
